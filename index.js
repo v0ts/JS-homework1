7df90bd452eb1a0776b916ed396348edb5f1e5c3
v0ts <cybulskij2011@ukr.net>
2024-05-17 18:22:39 +0300

    JS-HW1

---
+++ index.js
@@ -0,0 +1,37 @@
+let age = 13; 
+console.log(age);
+
+const myName = 'Viacheslav';
+console.log(myName);
+
+let isStudent = true; 
+console.log(isStudent);
+
+const myString = 'Життя — це те, що з тобою відбувається, поки ти будуєш плани. Джон Леннон';
+console.log(myString);
+
+let myNumber = 10;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const yourName = prompt("Введіть ваше і'мя");
+const welcome = alert('Вітаємо ' + yourName + '!');
+console.log(yourName);
+
+const confirmation =  confirm('Підтвердити?');
+if (confirmation === true){ 
+  alert('Дякую за підтвердження!');
+} else {
+  alert('Дію відмінено!');
+};
+
+alert('Ця дія небезпечна');
+const confirmations = confirm('Підтвердити?');
+if (confirmations === true){ 
+  alert('Дякую за підтвердження!');
+} else {
+  alert('Дію відмінено!');
+};
